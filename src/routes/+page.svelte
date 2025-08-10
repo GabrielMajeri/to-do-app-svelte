50c6413b2e5e1ccaf68c5c6da68fa1cfd4526ccb
Gabriel Majeri <gabriel.majeri6@gmail.com>
2025-08-10 19:28:14 +0300

    Fix formatting and set up auto format on save

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
-    const toDoList = [
-        {
-            id: 1,
-            text: "Do the dishes"
-        },
-        {
-            id: 2,
-            text: "Read a book"
-        },
-        {
-            id: 3,
-            text: "Learn Svelte"
-        }
-    ]
+  const toDoList = [
+    {
+      id: 1,
+      text: "Do the dishes",
+    },
+    {
+      id: 2,
+      text: "Read a book",
+    },
+    {
+      id: 3,
+      text: "Learn Svelte",
+    },
+  ];
 </script>
 
 <h1>To do app</h1>
 <p>My to do list:</p>
 <ul>
-    {#each toDoList as toDo (toDo.id)}
-        <li>{toDo.text}</li>
-    {/each}
+  {#each toDoList as toDo (toDo.id)}
+    <li>{toDo.text}</li>
+  {/each}
 </ul>
