879cb87fa8661c7c9566257334b0e9ff335bcedc
Gabriel Majeri <gabriel.majeri6@gmail.com>
2025-08-10 19:12:19 +0300

    Clear up landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>To do app</h1>
+<p>My to do list:</p>
+<ul>
+    <li>Do the dishes</li>
+    <li>Read a book</li>
+    <li>Learn Svelte</li>
+</ul>
