7d5b3a00887ad1226898126d62ee3768ace8bed3
Gabriel Majeri <gabriel.majeri6@gmail.com>
2025-08-10 19:25:58 +0300

    Refactor to do list to use in-memory array for to do items list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,24 @@
+<script lang="ts">
+    const toDoList = [
+        {
+            id: 1,
+            text: "Do the dishes"
+        },
+        {
+            id: 2,
+            text: "Read a book"
+        },
+        {
+            id: 3,
+            text: "Learn Svelte"
+        }
+    ]
+</script>
+
 <h1>To do app</h1>
 <p>My to do list:</p>
 <ul>
-    <li>Do the dishes</li>
-    <li>Read a book</li>
-    <li>Learn Svelte</li>
+    {#each toDoList as toDo (toDo.id)}
+        <li>{toDo.text}</li>
+    {/each}
 </ul>
